395d3e37587fe7e92d157999b1b35c7aee92015d
umesh kumar mehta <kumarumesh15294@gmail.com>
2022-03-19 07:44:28 +0530

    w3-resources-function-9

---
+++ index.js
@@ -0,0 +1,8 @@
+//9. Write a JavaScript function which accepts an argument and returns the type
+console.log(typeof "this is string"); /* string */
+console.log(typeof 1234); /* number */
+console.log(typeof []); /* object */
+console.log(typeof {}); /* object */
+console.log(typeof new Date()); /* object */
+console.log(typeof function () {}); /* function */
+console.log(typeof true); /* boolean */
